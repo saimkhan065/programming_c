9e37d6452f439e03617f8ef2d17e0a591e5aa917
Saim Khan <saimkhan065@gmail.com>
2023-03-07 18:48:21 -0500

    added enum

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World!\n"); // test print
+    enum myEnum  {pizza, lasagna, pasta}; // declare enum
+    enum myEnum foodItem1 = pizza;
+    enum myEnum foodItem2 = pasta; // declare enum vars + assign value
+    enum myEnum foodItem3 = lasagna;
+    printf("item 1 : %d\n", foodItem1); // print enum vars
+    printf("item 2 : %d\n", foodItem2);
+    printf("item 3 : %d\n", foodItem3);
     return 0;
 }
