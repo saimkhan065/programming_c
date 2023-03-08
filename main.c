db42b26d91e03a0be1aef6e59f91405ca760dea4
Saim Khan <saimkhan065@gmail.com>
2023-03-08 11:16:31 -0500

    added function and calls - area and pattern

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int areaRectangle(int length, int breadth); //function declaration
+void printPattern();
 
 int main() {
     printf("Hello, World!\n"); // test print
+
     enum myEnum  {pizza, lasagna, pasta}; // declare enum
     enum myEnum foodItem1 = pizza;
     enum myEnum foodItem2 = pasta; // declare enum vars + assign value
@@ -11,9 +13,21 @@ int main() {
     printf("item 1 : %d\n", foodItem1); // print enum vars
     printf("item 2 : %d\n", foodItem2);
     printf("item 3 : %d\n", foodItem3);
+
+    printf("area of rectangle: %d",areaRectangle(5, 10)); // function call
+    printPattern();
     return 0;
 }
 
 int areaRectangle(int length, int breadth){ //function definition
     return length*breadth;
+}
+
+void printPattern(){
+    for (int i = 0 ; i < 5 ; i++){
+        printf("\n");
+        for (int j = 0 ; j <= i ; j++){
+            printf("*");
+        }
+    }
 }
