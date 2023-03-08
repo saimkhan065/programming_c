18c91fbea54e8846c2a36cc2bbb2f89935b8f633
Saim Khan <saimkhan065@gmail.com>
2023-03-08 13:59:51 -0500

    added basic pointer declarations / definitions

--- main.c
+++ main.c
@@ -4,6 +4,9 @@ int areaRectangle(int length, int breadth); //function declaration
 void printPattern();
 
 int main() {
+    int number = 10;
+    int *pInt = &number; //declare int pointer and assign address of var number
+
     printf("Hello, World!\n"); // test print
 
     enum myEnum  {pizza, lasagna, pasta}; // declare enum
@@ -16,6 +19,9 @@ int main() {
 
     printf("area of rectangle: %d",areaRectangle(5, 10)); // function call
     printPattern();
+    //address - %p value - %d
+    printf("\npointer address: %p - pointer value: %p - value at pointer: %d", &pInt, pInt, *pInt);
+    printf("\nnumber address: %p", &number);
     return 0;
 }
 
